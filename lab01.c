6adf9d5b607697ff89df7640c4ff439c818e63e3
Fernando Quispe <fernando.quispe.27@unsch.edu.pe>
2026-09-09 23:26:15 -0500

    Implementacion de lectura dinamica con scanf

--- lab01.c
+++ lab01.c
@@ -8,5 +8,14 @@ int main() {
 	struct Proceso lista[5];
 	int n=5;
 	printf("planificador de procesos\n";
+	for (int i=0; i < n; i++) {
+		printf("\nProceso %d:\n",i+1);
+		printf("PID: ");
+		scanf("%d", &lista[i].pid);
+		printf("nombre: ");
+		scanf("%d", lista[i].nombre);
+		printf("Prioridad (1-5): ");
+		scanf("%d", &lista[i].prioridad);
+	}
 	return 0;
 }
